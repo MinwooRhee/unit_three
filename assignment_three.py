bd1b32b3ccfd120c8773a224de7fc7250fdc4206
Min Woo <minwoo@iMac5.local>
2018-09-25 14:00:09 -0400

    program works, need to use main() to organize

--- assignment_three.py
+++ assignment_three.py
@@ -1,19 +1,40 @@
 
+import turtle
 
-def main():
-    import turtle
+print("This program will draw a flower made of 7 hexagons with the user input of size and colors.")
 
-    print("This program will draw a flower made of 7 hexagons with the user input of size and colors.")
+length_side = input("Type the length of a side of a hexagon, in pixels: ")
 
-    length_side = input("Type the length of a side of a hexagon, in pixels: ")
+color_center = input("Type the color of the center, in HEX: ")
 
-    color_center = input("Type the color of the center, in HEX: ")
+color_petals = input("Type the color of the petals, in HEX: ")
 
-    color_petals = input("Type the color of the petals, in HEX: ")
 
-main()
+def draw_hexagon():
+    for x in range(6):
+        turtle.forward(float(length_side))
+        turtle.left(60)
 
-def draw_hexagon:
-    for x in range (6):
-        turtle.foward(float(length_side))
-        turtle.right(60)
+turtle.speed(10)
+
+turtle.color("black", color_center)
+
+turtle.begin_fill()
+
+draw_hexagon()
+
+turtle.end_fill()
+
+turtle.right(120)
+
+turtle.color("black", color_petals)
+
+for x in range(6):
+    turtle.begin_fill()
+    draw_hexagon()
+    turtle.end_fill()
+    turtle.right(120)
+    turtle.forward(float(length_side))
+    turtle.left(60)
+
+turtle.exitonclick()
