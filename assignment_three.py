061a181e891aad604e95c07341def3545ad3a532
Min Woo <minwoo@iMac5.local>
2018-09-26 14:37:22 -0400

    used return and main() function to organize the program. also added some comments

--- assignment_three.py
+++ assignment_three.py
@@ -1,40 +1,57 @@
 
 import turtle
 
-print("This program will draw a flower made of 7 hexagons with the user input of size and colors.")
 
-length_side = input("Type the length of a side of a hexagon, in pixels: ")
-
-color_center = input("Type the color of the center, in HEX: ")
-
-color_petals = input("Type the color of the petals, in HEX: ")
+def give_instruction():
+    print("This program will draw a flower made of 7 hexagons with the user input of size and colors.")
 
 
-def draw_hexagon():
+def input_length():
+    length_side = input("Type the length of a side of a hexagon, in pixels: ")
+    return length_side
+
+
+def input_color_center():
+    color_center = input("Type the color of the center, in HEX: ")
+    return color_center
+
+
+def input_color_petals():
+    color_petals = input("Type the color of the petals, in HEX: ")
+    return color_petals
+
+
+def draw_hexagon(length_side, color):
+    turtle.color("black", color)
+    turtle.begin_fill()
     for x in range(6):
         turtle.forward(float(length_side))
         turtle.left(60)
-
-turtle.speed(10)
-
-turtle.color("black", color_center)
-
-turtle.begin_fill()
-
-draw_hexagon()
-
-turtle.end_fill()
-
-turtle.right(120)
-
-turtle.color("black", color_petals)
-
-for x in range(6):
-    turtle.begin_fill()
-    draw_hexagon()
     turtle.end_fill()
-    turtle.right(120)
-    turtle.forward(float(length_side))
-    turtle.left(60)
 
-turtle.exitonclick()
+
+def main():
+    give_instruction()
+    length_side = input_length()
+    color_center = input_color_center()
+    color_petals = input_color_petals()
+
+    turtle.speed(10)
+
+    draw_hexagon(length_side, color_center)
+
+    turtle.right(120)
+    # rearranging where turtle is facing before starting the loop
+
+    for x in range(6):
+        draw_hexagon(length_side, color_petals)
+
+        turtle.right(120)
+        turtle.forward(float(length_side))
+        turtle.left(60)
+        # rearranging turtle before drawing the next petal
+
+    turtle.exitonclick()
+
+
+main()
