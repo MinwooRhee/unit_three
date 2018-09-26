358b892d097784fc72fc82eed7c922178194163e
Min Woo <minwoo@iMac5.local>
2018-09-26 14:06:10 -0400

    information returned from a function

---
+++ d3_unit_three_warmups.py
@@ -0,0 +1,14 @@
+
+def area_of_circle(radius):
+    area = 3.14 * radius ** 2
+    return area
+
+
+def main():
+    height = 9
+    radius = 12
+    volume_cylinder = area_of_circle(radius) * height
+    print(volume_cylinder)
+
+
+main()
